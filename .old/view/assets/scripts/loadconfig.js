854a423676ab4849251d6129d3f8aacbd66a1450
Broken_Deer <kxscluabcbook@outlook.com>
2022-11-21 10:06:37 +0800

    每日提交: 2022-11-21 10:07

--- .old/view/assets/scripts/loadconfig.js
+++
@@ -1,8 +0,0 @@
-const f = require("fs");
-function test() {
-    f.writeFile(
-        "test.txt",
-        "字字字字字字字字字字字字字字字字字字字字字字字字字字字字字字字字字",
-        (_error) => {}
-    );
-}
---
+++ view/assets/scripts/onload.js
@@ -0,0 +1,64 @@
+window.onload = function () {
+    $("body").attr("style", "transform: scale(1); opacity: 1; transition: transform .4s ease, opacity .4s ease");
+
+    $("#user_add").bind("click", () => {});
+    $("#start_game").bind("click", () => {
+        display_btn();
+        $("#main").empty();
+    });
+    $("#version_list").bind("click", () => {});
+    $("#download").bind("click", () => {
+        change_page("#download_pg");
+        version_list("vanilla");
+    });
+    $("#v_setting").bind("click", () => {
+        test();
+    });
+    $("#g_setting").bind("click", () => {
+        change_page("#g_setting_pg");
+        setTimeout(() => {
+            slider("CA028F76", "memory_value", 128, 16384);
+        }, 550);
+    });
+    if ($("#java_auto_choose").prop("checked")) {
+        $("#E16616C6").addClass("disable");
+    } else {
+        $("#E16616C6").removeClass("disable");
+    }
+    if ($("#automatic_memory").prop("checked")) {
+        $("#D3E268A7").addClass("disable");
+    } else {
+        $("#D3E268A7").removeClass("disable");
+    }
+    /*   const webview = document.querySelector("webview");
+  webview.addEventListener("dom-ready", () => {
+    webview.openDevTools();
+  }); */
+    zh_cn();
+    $("#2B39A329").click(function () {
+        const a = document.querySelectorAll(".crafting_table");
+        const b = document.querySelectorAll(".ancient_debris");
+        if ($("#2B39A329").prop("checked")) {
+            $(a).removeClass("dispnone");
+            $(b).removeClass("dispnone");
+        } else {
+            $(a).addClass("dispnone");
+            $(b).addClass("dispnone");
+        }
+    });
+    $("#java_auto_choose").click(function () {
+        if ($("#java_auto_choose").prop("checked")) {
+            $("#E16616C6").addClass("disable");
+        } else {
+            $("#E16616C6").removeClass("disable");
+        }
+    });
+    $("#automatic_memory").click(function () {
+        if ($("#automatic_memory").prop("checked")) {
+            $("#D3E268A7").addClass("disable");
+        } else {
+            $("#D3E268A7").removeClass("disable");
+        }
+    });
+};
+document.addEventListener("DOMContentLoaded", () => {});
